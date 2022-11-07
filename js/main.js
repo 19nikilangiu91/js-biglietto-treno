75c078e46dd046cc929e02079bfdf15eda283563
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-07 15:32:27 +0100

    età

--- js/main.js
+++ js/main.js
@@ -15,12 +15,20 @@ let utenteEta = parseInt(prompt("Qual'è la sua età?"))
 
 console.log(utenteEta)
 
+let etaminore = 0 < 18;
+
+let adulto = 18 < 65;
+
+let anziano = 65 < 100;
 
 // Se il cliente è minorenne verrà applicato lo sconto del 20%
 
 
+// let minorePrezzoKm = (prezzoKm * 80) / 100;
+
 
 // Se il cliente è adulto pagherà il prezzo pieno
+
 
 // Se il cliente è anziano verrà applicato lo sconto del 40%
 
