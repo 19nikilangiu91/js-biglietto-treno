b600e604ba6b6db49f3e14af2efa0ce77898a809
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-07 19:45:37 +0100

    Bonus

---
+++ Bonus/js/main.js
@@ -0,0 +1,55 @@
+
+const prezzoKm = 0.21;
+
+
+console.log(prezzoKm)
+
+// Variabili
+
+let utenteKm, utenteEta, sconto;
+
+
+// Quanti chilometri vuole percorrere l'utente?
+
+utenteKm = parseInt(prompt("Quanti chilometri vuole percorrere?"))
+
+console.log(utenteKm)
+
+// Quanti anni ha l'utente?
+
+utenteEta = parseInt(prompt("Qual'è la sua età?"))
+
+console.log(utenteEta)
+
+sconto = 0;
+
+if (utenteEta < 18){
+    sconto = 20;
+} else if (utenteEta >= 65){
+    sconto = 40;
+}
+
+console.log(sconto)
+
+// Calcolare il prezzo totale del biglietto
+
+const prezzoTotale = prezzoKm * utenteKm;
+
+console.log(prezzoTotale)
+
+// Calcolare il prezzo scontanto del biglietto
+
+const prezzoScontato = prezzoTotale * (100 - sconto) / 100;
+
+console.log(prezzoScontato)
+
+// Output
+
+document.getElementById("divone").innerHTML= `Con l'acquisto di questo biglietto,
+potrà percorrere ${utenteKm}Km.`
+
+document.getElementById("divtwo").innerHTML= `Avendo ${utenteEta} anni, avrà il diritto di uno
+sconto del ${sconto}%, il suo prezzo sarà di ${prezzoTotale}€`
+
+document.getElementById("divthree").innerHTML= `Il suo prezzo scontato totale sarà di ${prezzoScontato}€.`
+
