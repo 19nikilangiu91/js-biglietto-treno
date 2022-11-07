e7952d2789ba6ee389b2d488eaa11e3b11ceb464
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-07 14:41:11 +0100

    Upgrade Comments

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+// Quanti chilometri vuole percorrere l'utente
+
+// Quanti anni ha l'utente
+
+// Se il cliente è minorenne verrà applicato lo sconto del 20%
+
+// Se il cliente è adulto pagherà il prezzo pieno
+
+// Se il cliente è anziano verrà applicato lo sconto del 40%
+
+// Output
+
