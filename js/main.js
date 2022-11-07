2005c7dc7d9077b6f81c51308980d474621b32bb
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-07 16:35:59 +0100

    Quanti anni ha l'utente

--- js/main.js
+++ js/main.js
@@ -3,28 +3,30 @@ const prezzoKm = 0.21
 console.log(prezzoKm)
 
 
-// Quanti chilometri vuole percorrere l'utente
+// Quanti chilometri vuole percorrere l'utente?
 
 let utenteKm = parseInt(prompt("Quanti chilometri vuole percorrere?"))
 
 console.log(utenteKm)
 
-// Quanti anni ha l'utente
+// Quanti anni ha l'utente?
 
 let utenteEta = parseInt(prompt("Qual'è la sua età?"))
 
 console.log(utenteEta)
 
-let etaminore = 0 < 18;
+let sconto = 0;
 
-let adulto = 18 < 65;
+if (utenteEta < 18){
+    sconto = 20;
+} else if (utenteEta >= 65){
+    sconto = 40;
+}
 
-let anziano = 65 < 100;
+console.log(sconto)
+
 
 // Se il cliente è minorenne verrà applicato lo sconto del 20%
-
-
-// let minorePrezzoKm = (prezzoKm * 80) / 100;
 
 
 // Se il cliente è adulto pagherà il prezzo pieno
